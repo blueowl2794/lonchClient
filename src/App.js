c6268afe8700b46903911f29c02aa395072f64f8
erik santiago <blueowl2794@gmail.com>
2022-02-28 13:12:05 -0500

    cambioenapp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Switch, Route } from 'react-router-dom';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Landing from './components/Landing';
@@ -11,10 +11,25 @@ import DetailMenu from './components/DetailMenu';
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<> <Landing/> <Steps/> <Menu/> <Contact/> <Footer/> </>} />
-        <Route path="/products/:id" element={<DetailMenu/>} />
-      </Routes>
+      <Landing/>
+      <Steps/>
+      <Menu/>
+      <Contact/>
+      <Footer/>
+      <Switch>
+      <Route path="/" element={ <Landing/>} />
+
+      </Switch>
+
+      {/* // <Routes> */}
+      {/* <Route path="/" element={ <Landing/>} /> */}
+      {/* <Route path="/" element={ <Steps/>  } />
+      <Route path="/" element={ <Menu/> } />
+      <Route path="/" element={ <Contact/>} />
+      <Route path="/" element={ <Footer/> } /> */}
+        {/* <Route path="/" element={<> <Landing/> <Steps/> <Menu/> <Contact/> <Footer/> </>} /> */}
+      {/* //   <Route path="/products/:id" element={<DetailMenu/>} /> */}
+      {/* // </Routes> */}
     </div>
   );
 }
